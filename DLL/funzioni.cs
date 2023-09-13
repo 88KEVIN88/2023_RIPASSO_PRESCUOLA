80b2eb1524916779d23ef4304c8da83c73287698
azzol <azzol@PC001>
2023-09-13 20:34:52 +0200

    Add project files.

---
+++ DLL/funzioni.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace DLL
+{
+   
+    public class funzioni
+    {
+       public string  filecsv = @"D:\Programmi\Ripasso_2023\Ripasso_2023\bin\Debug\ciao.csv";
+        public int cont;
+        public char delimiter = ';';
+        public  void Add() //Aggiunta di un campo alla fine di ogni record
+        {
+            Random random = new Random();
+            string[] csvline = File.ReadAllLines(filecsv);
+
+            for(int i = 0; i < csvline.Length; i++)
+            {
+                csvline[i] += $";Mio valore:{random.Next(10, 21)};0";
+            }
+            File.WriteAllLines(filecsv, csvline);
+        }
+        public  int  Cont()
+        {
+            using (StreamReader rd = new StreamReader(filecsv))
+            {
+                string hdline= rd.ReadLine();
+                if(!String.IsNullOrEmpty(hdline))
+                {
+                    cont = hdline.Split(delimiter).Length;
+                }
+                
+            }
+            return cont;
+        }
+    }
+}
